fd33765938fc2dc3932258d78705e62955363c0f
Olena-Rudnieva <rudnievao@gmail.com>
2023-03-21 16:34:30 +0200

    Add slick slider js

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,5 @@ import './modal.js';
 import 'https://code.jquery.com/jquery-3.6.4.min.js';
 import './alt-play-btn';
 import './slick.js';
-import './slider.js'
+import './slider.js';
+import './review-slider';
