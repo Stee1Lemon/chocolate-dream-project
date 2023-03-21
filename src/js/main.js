caac5301639a5a23b0f19483a1ea7557d8a07221
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2023-03-21 14:10:07 +0000

    js

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,5 @@ import './modal.js';
 // import './alt-play-btn';
 // import 'https://code.jquery.com/jquery-3.6.4.min.js';
 import './slick.min.js'
-import './slick.js';
+// import './slick.js';
 import './slider.js';
