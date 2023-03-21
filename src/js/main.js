a779507165b421e56baaaf4d2e5c0237e0950223
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2023-03-21 13:27:04 +0000

    test

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import './mobile-menu.js';
 import './modal.js';
-import './alt-play-btn';
+// import './alt-play-btn';
 import 'https://code.jquery.com/jquery-3.6.4.min.js';
 import './slick.min.js'
 import './slick.js';
